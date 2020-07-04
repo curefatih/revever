8e31847c7686a1236bd03e1358834a518a199934
curefatih <curefat@gmail.com>
2020-07-04 13:57:52 +0300

    add commit list to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,37 @@ import './App.scss';
 import Sidebar from './components/Sidebar/Sidebar';
 import CodeEditor from './components/CodeEditor/CodeEditor';
 import SourceOpenSteps from './components/SourceOpenSteps/SourceOpenSteps';
+import CommitCard from './components/CommitCard/CommitCard';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import CommitList from './components/CommitList/CommitList';
 
 function App() {
   return (
-    <div className="app wrap xl-flexbox xl-top">
-      <Sidebar className="col" />
-      <div className="col content">
-        <div className="wrap xl-flexbox xl-middle xl-center">
-          <SourceOpenSteps className="col xl-1-2" />
+    <Provider store={store}>
+      <div className="app wrap xl-flexbox xl-top">
+        <Sidebar className="sidebar_wrapper col" />
+        <div className="col content">
+          <CommitList />
+          {/* <div className="wrap xl-flexbox xl-middle xl-center">
+            <div className="col xl-7-12 commit-card_wrapper" style={{ padding: "10px" }}>
+              <CommitCard tree="123" message="initial commit" />
+            </div>
+            <div className="col xl-2-12 point"></div>
+
+          </div>
+          <div className="wrap xl-flexbox xl-middle xl-center">
+            <div className="col xl-7-12 commit-card_wrapper" style={{ padding: "10px" }}>
+              <CommitCard tree="123" message="awesome commit" />
+            </div>
+            <div className="col xl-2-12 point"></div>
+
+          </div> */}
+
+
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
