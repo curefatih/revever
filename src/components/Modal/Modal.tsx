76ea30347c454600fa7ef9cbc8e5023ac05f829f
curefatih <curefat@gmail.com>
2020-07-04 13:56:31 +0300

    update modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,17 +4,35 @@ import "./Modal.scss";
 
 
 let modalWrapper: HTMLElement;
-function Modal({ children, display }: { children: ReactElement, display?: boolean }) {
+function Modal(
+  { children, onClose }:
+    {
+      children: ReactElement,
+      onClose?: Function
+    }) {
 
   let modal = document.createElement("div");
-  modal.classList.add("modal-content", "col", "xl-1-4");
+  modal.classList.add("modal-content", "col", "xl-1-2");
 
   useEffect(() => {
     const parent = document.body;
     modalWrapper = document.createElement("div");
     modalWrapper.classList.add("modal-custom_wrapper", "wrap", "xl-flexbox", "xl-middle", "xl-center");
-    modalWrapper.style.opacity = display ? "1" : "0";
+
+    const modalCloserDiv = document.createElement("div");
+    modalCloserDiv.classList.add("modal-closer")
+    modalCloserDiv.style.zIndex = "998";
+
+    modalWrapper.appendChild(modalCloserDiv);
+    if (onClose)
+      modalCloserDiv.addEventListener("click", (e) => {
+        console.log(e.currentTarget, e.target);
+
+        if (e.currentTarget === e.target) onClose(e)
+      });
+
     parent.appendChild(modalWrapper);
+    modal.style.zIndex = "999";
     modalWrapper.appendChild(modal);
     return () => { parent.removeChild(modalWrapper) }
   }, [])
