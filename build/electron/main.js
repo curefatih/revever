0df7cbb2b1fc73ebd1270e914315c10d6b697bfe
curefatih <curefat@gmail.com>
2020-07-08 15:54:29 +0300

    update build

--- build/electron/main.js
+++ build/electron/main.js
@@ -1,6 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
 var electron_1 = require("electron");
+var path = require("path");
+// import getRepo from './functions/getRepo';
+var getLogs_1 = require("./functions/getLogs");
+var getFiles_1 = require("./functions/getFiles");
+// import getRepo from './functions/getRepo';
 var win;
 var winURL = process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
@@ -10,15 +51,20 @@ function createWindow() {
     win = new electron_1.BrowserWindow({
         width: 1424,
         height: 720,
+        // minHeight: 230,
+        // minWidth: 540,
         webPreferences: {
             nodeIntegration: true,
             preload: __dirname + '/preload.js'
-        }
+        },
+        frame: process.env.NODE_ENV === "development" ? true : false,
+        icon: path.join(__dirname, '/favicon_64x64.png')
     });
     // and load the index.html of the app.
     win.loadURL(winURL);
     // Open the DevTools.
-    win.webContents.openDevTools();
+    if (process.env.NODE_ENV === "development")
+        win.webContents.openDevTools();
 }
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -41,11 +87,68 @@ electron_1.app.on('activate', function () {
 });
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-electron_1.ipcMain.on('select-dir', function (event, arg) {
-    console.log("SELECT DIR", arg);
-    // const result = await dialog.showOpenDialog(win, {
-    //   properties: ['openDirectory']
-    // })
-    // console.log('directories selected', result.filePaths)
-    event.returnValue = "henlo";
-});
+electron_1.ipcMain.on('add-source-dir', function (event, arg) { return __awaiter(void 0, void 0, void 0, function () {
+    var result;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                console.log("SELECT DIR", arg);
+                return [4 /*yield*/, electron_1.dialog.showOpenDialog(win, {
+                        properties: ['openDirectory']
+                    })];
+            case 1:
+                result = _a.sent();
+                console.log('directories selected', result.filePaths);
+                if (!result.filePaths.length) {
+                    event.returnValue = { status: 0, message: "No folder selected" };
+                    return [2 /*return*/];
+                }
+                // TODO: check data first and return if its valid.
+                event.returnValue = { status: 1, message: "Opened folder", data: result.filePaths[0] };
+                return [2 /*return*/];
+        }
+    });
+}); });
+electron_1.ipcMain.on('get-logs', function (event, repoPath) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log("getLogs", repoPath);
+        // const result = await dialog.showOpenDialog(win, {
+        //   properties: ['openDirectory']
+        // })
+        // console.log('directories selected', result.filePaths)
+        // if (!result.filePaths.length) {
+        //   event.returnValue = { status: 0, message: "No folder selected" }
+        //   return;
+        // }
+        getLogs_1["default"](repoPath)
+            .then(function (res) {
+            event.returnValue = { status: 1, message: "Success", data: res };
+        })["catch"](function (err) {
+            event.returnValue = {
+                status: -1,
+                message: "Error while opening reposity:" + err.message
+            };
+        });
+        return [2 /*return*/];
+    });
+}); });
+electron_1.ipcMain.on('get-files', function (event, repo) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log("getFiles", repo);
+        // const result = await dialog.showOpenDialog(win, {
+        //   properties: ['openDirectory']
+        // })
+        // console.log('directories selected', result.filePaths)
+        // if (!result.filePaths.length) {
+        //   event.returnValue = { status: 0, message: "No folder selected" }
+        //   return;
+        // }
+        getFiles_1["default"](repo.path, repo.sha)
+            .then(function (res) {
+            event.returnValue = { status: 1, message: "Success", data: res };
+        })["catch"](function (err) {
+            event.returnValue = { status: 0, message: "Error while getting files:" + err.message };
+        });
+        return [2 /*return*/];
+    });
+}); });
